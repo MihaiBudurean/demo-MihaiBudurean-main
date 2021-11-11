fe7c7e96a739c1c93d404b213011dfe1f6f60e9b
MihaiBudurean <marius.budurean01@e-uvt.ro>
2021-11-11 13:07:53 +0200

    commit on https://github.com/programming3/demo-MihaiBudurean

---
+++ src/main/java/ro/uvt/p3/lab7a/Example.java
@@ -0,0 +1,19 @@
+package ro.uvt.p3.lab7a;
+
+public class Example {
+    String name;
+    String text;
+
+    public Example(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
